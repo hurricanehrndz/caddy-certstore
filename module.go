b67030dda6b57ccbe46719eeaa3d6e910fa0c0ae
Carlos Hernandez <carlos@hrndz.ca>
2025-11-01 21:11:39 -0600

    fix: logging, nil checks, infinite recursion

--- module.go
+++ module.go
@@ -41,12 +41,16 @@ func (h *HTTPTransport) Provision(ctx caddy.Context) error {
 
 	// Validate that Name is set
 	if h.ClientCertificateMatcher.Name == "" {
-		return fmt.Errorf("client_certificate_match must have either 'name'")
+		return fmt.Errorf("client_certificate_match must set 'name' property")
 	}
 
 	clientCert, err := h.ClientCertificateMatcher.getCertificate()
 	if err != nil {
-		return fmt.Errorf("no client certificate found in: %s with common name:", h.ClientCertificateMatcher.Location, h.ClientCertificateMatcher.Name)
+		return fmt.Errorf("no client certificate found in: %s with common name: %s", h.ClientCertificateMatcher.Location, h.ClientCertificateMatcher.Name)
+	}
+
+	if h.Transport.TLSClientConfig == nil {
+		h.Transport.TLSClientConfig = new(tls.Config)
 	}
 	h.Transport.TLSClientConfig.Certificates = []tls.Certificate{clientCert}
 
@@ -56,9 +60,11 @@ func (h *HTTPTransport) Provision(ctx caddy.Context) error {
 // Cleanup implements caddy.CleanerUpper, closes any idle connections.
 // and frees any allocations from accessing certificate store
 func (h *HTTPTransport) Cleanup() error {
-	defer h.ClientCertificateMatcher.cleanup()
+	if h.ClientCertificateMatcher != nil {
+		defer h.ClientCertificateMatcher.cleanup()
+	}
 
-	err := h.Cleanup()
+	err := h.HTTPTransport.Cleanup()
 	if err != nil {
 		return err
 	}
